8b40fa5bee5e53714c16c4e19d7f201ceb563493
Joao Pedro <jp.ssilva@live.com>
2020-04-24 10:26:23 -0300

    Introdução em Thread

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading;
 
-namespace Threads
+namespace _01_Thread
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instaciar a thread
+            Thread thread = new Thread(ThreadRepeticao);
+
+            //Inicar a thread
+            thread.Start();
+
+            //Comprovar que há o processamento em paralelo
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Main: " + i);
+            }
+
+            Console.ReadKey();
+        }
+
+        //Ao se utilizar IO - Tela, Rede, Armazenamento 
+        //Existe uma demora no processo
+        static void ThreadRepeticao()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Número: " + i);
+            }
         }
     }
 }
